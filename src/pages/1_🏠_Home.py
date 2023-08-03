7c3c83a3826acd4df565e082d1349bffd020e0c7
jimmily98 <lianghangao98@gmail.com>
2023-08-03 16:14:06 +0200

    updated

--- src/pages/1_🏠_Home.py
+++ src/pages/1_🏠_Home.py
@@ -24,40 +24,38 @@ st.markdown('<center> \
 </center> ')
 st.markdown('Sur l\'image ci-dessus, la puissance au point 2 est approximé par la puissance moyenne entre le point 1 et le point 3 où deux processus sont pris en compte. D\'une part, l\'énergie nécessaire pour abaisser la température de l\'air à l\'intérieur de la caisse, et d\'autre part, la chaleur transférée à travers du parois. La somme de l\'énergie de ces deux processus est l\'énergie fournie par la machine frigorifique.')
 st.markdown('L\'énergie pour baisser la température intérieure peut être obtenue à partir de la capacité thermique massique et le décalage de température entre ces deux points :')
-st.markdown('$$ \
-\\begin{align} \
-&Q_1 = (m_{air}+m_{wall})\*c\*\Delta T_1 \\ \
+st.latex(r''' \
+\begin{align} \
+&Q_1 = (m_{air}+m_{wall})*c*\Delta T_1 \\ \
 \\ \
-&\\text{$m_{air}$: masse d\'air à l\'intérieur}\\ \
-&\\text{$m_{wall}$: masse d\'isolant}\\ \
-&\\text{c: capacité thermique de l\'air} \
-\\end{align} \
-$$')
+&\text{$m_{air}$: masse d'air à l'intérieur}\\ \
+&\text{$m_{wall}$: masse d'isolant}\\ \
+&\text{c: capacité thermique de l'air} \
+\end{align} \
+''')
+
 st.markdown('L\'énergie de la conduction thermique est obtenue à partir du théorème de Fourier. Sur chacune des 6 surfaces intérieures, l\'énergie conductive est: ')
-st.markdown('$$ \
-\\begin{align} \
-&q = \frac{\\lambda\*S*\\Delta T}{L}*time \\ \
+st.latex(r''' \
+\begin{align} \
+&q = \frac{\lambda\*S*\Delta T}{L}*time \\ \
 \\ \
-&\\text{$\lambda$: coefficient de la conductivité thermique}\\ \
-&\\text{S: aire de surface}\\ \
-&\\text{L: épaisseur du parois}\\ \
-&\\text{$\Delta T$: décalage de température, supposé à rester stable} \
-\\end{align} \
-$$')
+&\text{$\lambda$: coefficient de la conductivité thermique}\\ \
+&\text{S: aire de surface}\\ \
+&\text{L: épaisseur du parois}\\ \
+&\text{$\Delta T$: décalage de température, supposé à rester stable} \
+\end{align} \
+''')
 
 st.markdown('Sur l\'image au-dessus, $\Delta T$ est traité comme le décalage moyenne de ces 3 points, à savoir :')
-st.markdown('$$')
-st.markdown('\\begin{align}')
-st.markdown('\\Delta T &= \\frac{\\Delta T_{21} + 2*\\Delta T_{22} + \\Delta T_{23}}{4}\\')
-st.markdown('\\text{time} &= 2*\\Delta t')
-st.markdown('\\end{align}')
-st.markdown('$$')
+st.latex(r'''\begin{align}' \
+    \Delta T &= \\frac{\Delta T_{21} + 2*\Delta T_{22} + \Delta T_{23}}{4}\\ \
+    \text{time} &= 2*\Delta t')
+    \end{align}''')
 st.markdown('Supposons que les énergies de conduction ainsi obtenues sont numérotées $q_1, q_2, q_3, q_4, q_5, q_6$ sur les 6 surfaces intérieures, alors l\'énergie de conduction totale est la somme de ces six énergies : ')
-st.markdown('$$ \
-Q_2 = \\sum_{i=1}^6 q_i \
+st.latex(r''' \
+Q_2 = \sum_{i=1}^6 q_i \
 $$ \
 La puissance approximée de la machine frigorique au point 2 est donc : \
 $$ \
-\\bar{P} = \\frac{Q_1+Q_2}{2\\Delta t} \
-$$ \
-')
+\bar{P} = \frac{Q_1+Q_2}{2\Delta t} \
+''')
