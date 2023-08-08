58f8e017c3c1fd2c7a54c41967f651a18baa90bc
jimmily98 <lianghangao98@gmail.com>
2023-08-08 16:23:33 +0200

    changes merged

--- src/pages/2_📁_Data.py
+++ src/pages/2_📁_Data.py
@@ -46,28 +46,4 @@ else:
         st.chat_message('Please upload your parameters OR use default parameters')
         st.session_state['df_par'] = 0
 
-
-### --- draw the data --- ###
-# read the data from 3-26 column, from 8th row to the end
-data = df.iloc[7:, 2:26]
-# draw the data of each column
-duration = len(data)
-for i in range(24):
-    plt.plot(list(range(0,duration*2,2)), data.iloc[:, i].astype(float), label=str(i+1))
-plt.xlabel("time")
-plt.ylabel("temp")
-plt.legend()
-plt.title("data_" + filename)
-plt.show()
-### ------------------- ###
-
-
-### --- extract information --- ###       
-# Client 
-# client_list = []
-# for num in np.append(np.arange(26,33),np.arange(38,47)):
-#     filename = 'F33' + str(num) + '.xlsx'
-#     df = pd.read_excel(filename,decimal=',',header=None)
-#     client_list.append(df.iloc[2, 2])
-# print("Clients: ", np.unique(client_list))
     
