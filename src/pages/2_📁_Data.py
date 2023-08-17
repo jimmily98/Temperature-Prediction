959d801c68a8ef25826384a4ec60844c2a39be42
jimmily98 <lianghangao98@gmail.com>
2023-08-17 16:05:59 +0200

    updated

--- src/pages/2_📁_Data.py
+++ src/pages/2_📁_Data.py
@@ -37,9 +37,10 @@ with col2:
     )
     uploaded_file = col2.file_uploader("Choose a file")
     st.session_state['uploaded_file'] = uploaded_file
-    df_prt = pd.read_excel(uploaded_file,decimal='.',header=None)
-    st.write(df_prt)
-    option.options = df_prt[:,0]
+    if uploaded_file is not None:
+        df_prt = pd.read_excel(uploaded_file,decimal='.',header=None)
+        st.write(df_prt)
+        option.options = df_prt[:,0]
 
 
 if deflt:
