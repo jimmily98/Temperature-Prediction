1eb31ed079ed1ba9f35ce69ecc11d0f9fd778ddf
jimmily98 <lianghangao98@gmail.com>
2023-08-18 12:02:53 +0200

    updated

--- src/pages/4_🥝_About.py
+++ src/pages/4_🥝_About.py
@@ -7,6 +7,6 @@ st.markdown("<h1 style='text-align: center;'>About</h1>", unsafe_allow_html=True
 st.markdown('<br></br>', unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'> Contact </h3>", unsafe_allow_html=True)
 st.markdown('<br></br>', unsafe_allow_html=True)
-st.markdown('Credit: <a href="https://www.cemafroid.fr">https://www.cemafroid.fr</a>',unsafe_allow_html=True)
 st.markdown('Tel: (+33) 0640994867', unsafe_allow_html=True)
-st.markdown('Email: <a href="mailto:lianghangao98@gmail.com">lianghangao98@gmail.com</a>',unsafe_allow_html=True)
+st.markdown('Email: <a href="mailto:lianghangao98@gmail.com" style=\'text-align: center;\'>lianghangao98@gmail.com</a>',unsafe_allow_html=True)
+st.markdown('Credit: <a href="https://www.cemafroid.fr">https://www.cemafroid.fr</a>',unsafe_allow_html=True)
