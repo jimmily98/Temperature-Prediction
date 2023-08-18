89665605809b3313717193a44ead123f960f0765
jimmily98 <lianghangao98@gmail.com>
2023-08-18 12:10:04 +0200

    updated

--- src/pages/4_🥝_About.py
+++ src/pages/4_🥝_About.py
@@ -5,8 +5,7 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>About</h1>", unsafe_allow_html=True)
 st.markdown('<br></br>', unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'> Contact </h3>", unsafe_allow_html=True)
-st.markdown('<br></br>', unsafe_allow_html=True)
+st.markdown("<h3'> Contact </h3>", unsafe_allow_html=True)
 st.markdown('<h6 style="text-align: center;">Tel: (+33) 0640994867</h6>', unsafe_allow_html=True)
 st.markdown('<h6 style="text-align: center;">Email: <a href="mailto:lianghangao98@gmail.com">lianghangao98@gmail.com</a></h6>',unsafe_allow_html=True)
 st.markdown('<h6 style="text-align: center;">Credit: <a href="https://www.cemafroid.fr">https://www.cemafroid.fr</a></h6>',unsafe_allow_html=True)
