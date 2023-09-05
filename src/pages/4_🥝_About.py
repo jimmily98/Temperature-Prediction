73f2bf39c392b9de36cb0bbba2a30f8784a4ac16
jimmily98 <lianghangao98@gmail.com>
2023-09-06 00:23:49 +0200

    updated

--- src/pages/4_🥝_About.py
+++ src/pages/4_🥝_About.py
@@ -5,7 +5,7 @@ import streamlit as st
 from PIL import Image
 
 col1, col2 = st.columns([0.4,0.6])
-st.markdown("<h1 style='text-align: center;'>About</h1>", unsafe_allow_html=True)
+st.markdown("<h1>About</h1>", unsafe_allow_html=True)
 st.markdown('<br></br>', unsafe_allow_html=True)
 with col1:
     st.markdown('<h4 style="margin-left: 75px;"> Contact </h4>', unsafe_allow_html=True)
