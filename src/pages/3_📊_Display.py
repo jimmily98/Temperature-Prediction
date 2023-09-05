609f9996a8960451bb480eba685ffa435b40ff50
jimmily98 <lianghangao98@gmail.com>
2023-09-06 00:10:41 +0200

    updated

--- src/pages/3_📊_Display.py
+++ src/pages/3_📊_Display.py
@@ -89,7 +89,7 @@ if prsd:
     with col1:
         st.markdown("<h4 style='text-align: center;'> Raw data</h4>", unsafe_allow_html=True)
         ### ------------------- Read Time-Temperature data --------------------------------------
-        duration = len(data)
+        duration = windowbar-1
         # st.write(data.iloc[:, 0:12].mean(axis = 1).astype(float))
         plt.plot(list(range(0,duration*2,2)), data.iloc[:, 0:12].mean(axis = 1), label="external temp")
         plt.plot(list(range(0,duration*2,2)), data.iloc[:,12:24].mean(axis = 1), label="internal temp")
