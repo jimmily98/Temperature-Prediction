d2f08184f23ce1625483afc8d9dbbc91cdaf4b3a
jimmily98 <lianghangao98@gmail.com>
2023-09-06 00:24:40 +0200

    updated

--- src/pages/4_🥝_About.py
+++ src/pages/4_🥝_About.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import streamlit as st
 from PIL import Image
 
-col1, col2 = st.columns([0.4,0.6])
-st.markdown("<h1>About</h1>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>About</h1>", unsafe_allow_html=True)
 st.markdown('<br></br>', unsafe_allow_html=True)
+col1, col2 = st.columns([0.4,0.6])
 with col1:
     st.markdown('<h4 style="margin-left: 75px;"> Contact </h4>', unsafe_allow_html=True)
     st.markdown('<h6 style="text-align: left;">Tel: (+33) 0640994867</h6>', unsafe_allow_html=True)
